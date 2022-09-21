1ab5a9a74e19693a5fad58ed91ec4494a298bbbf
BetterthanDrugs <BetterthanDrugs@users.noreply.github.com>
2022-09-22 00:52:28 +0200

    Update app.component.ts
    complete

--- angular-1/src/app/app.component.ts
+++ angular-1/src/app/app.component.ts
@@ -8,6 +8,7 @@ import {
   Subject,
   Subscription,
   switchMap,
+  debounceTime,
 } from 'rxjs';
 import { MockDataService } from './mock-data.service';
 
@@ -31,9 +32,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
   changeCharactersInput(element): void {
     // 1.1. Add functionality to changeCharactersInput method. Changes searchTermByCharacters Subject value on input change.
-    const inputValue: string = element.target.value;
     // YOUR CODE STARTS HERE
-
+    const inputValue: string = element.target.value;
+    this.searchTermByCharacters.subscribe();
+    this.searchTermByCharacters.next(inputValue);
     // YOUR CODE ENDS HERE
   }
 
@@ -43,19 +45,27 @@ export class AppComponent implements OnInit, OnDestroy {
     // 2. Since we don't want to spam our service add filter by input value and do not call API until a user enters at least 3 chars.
 
     // 3. Add debounce to prevent API calls until user stop typing.
-
-    this.charactersResults$ = this.searchTermByCharacters
-      .pipe
+    this.charactersResults$ = this.searchTermByCharacters.pipe(
       // YOUR CODE STARTS HERE
-
-      // YOUR CODE ENDS HERE
-      ();
+      debounceTime(1500),
+      filter((value) => value.length >= 3),
+      switchMap((value) => this.mockDataService.getCharacters(value))
+    );
+    // YOUR CODE ENDS HERE
   }
 
   loadCharactersAndPlanet(): void {
     // 4. On clicking the button 'Load Characters And Planets', it is necessary to process two requests and combine the results of both requests into one result array. As a result, a list with the names of the characters and the names of the planets is displayed on the screen.
     // Your code should looks like this: this.planetAndCharactersResults$ = /* Your code */
     // YOUR CODE STARTS HERE
+    this.planetAndCharactersResults$ = combineLatest([
+      this.mockDataService.getCharacters(),
+      this.mockDataService.getPlatents(),
+    ]).pipe(
+      map((response) => {
+        return response.reduce((acc, val) => acc.concat(val), []);
+      })
+    );
     // YOUR CODE ENDS HERE
   }
 
@@ -66,12 +76,19 @@ export class AppComponent implements OnInit, OnDestroy {
     - Subscribe to changes
     - Check the received value using the areAllValuesTrue function and pass them to the isLoading variable. */
     // YOUR CODE STARTS HERE
+    combineLatest([
+      this.mockDataService.getCharactersLoader(),
+      this.mockDataService.getPlanetLoader(),
+    ]).subscribe((response) => {
+      this.isLoading = response.every((item) => !item) ? false : true;
+    });
     // YOUR CODE ENDS HERE
   }
 
   ngOnDestroy(): void {
     // 5.2 Unsubscribe from all subscriptions
     // YOUR CODE STARTS HERE
+    this.searchTermByCharacters.unsubscribe();
     // YOUR CODE ENDS HERE
   }
 
